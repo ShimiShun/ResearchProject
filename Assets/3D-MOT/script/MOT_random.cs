722fffe3ba07f3e4db196fc703750429168db6d1
ShimiShun <b1013077@gmail.com>
2017-08-30 23:05:33 +0900

    first commit

---
+++ Assets/3D-MOT/script/MOT_random.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOT_random : MonoBehaviour {
+
+
+	public GameObject wall;
+	public GameObject maincamera;
+
+	// Use this for initialization
+	void Start () {
+
+		wall.transform.localScale = new Vector3 (110, 60, 10);
+		//maincamera.transform.localPosition=new Vector2 (Screen.width / 2, Screen.height / 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
