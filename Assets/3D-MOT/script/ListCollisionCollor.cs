5bca2963526805ddd98bc7bcd045d966df1c3291
ShimiShun <b1013077@gmail.com>
2017-09-13 16:38:17 +0900

    あああ

---
+++ Assets/3D-MOT/script/ListCollisionCollor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListCollisionCollor : MonoBehaviour {
+
+	public List<string> _colArray;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (_col);
+	}
+
+//	public void OnCollisionEnter(Collision collision){
+//		//Destroy(collision.gameObject);
+//		if (collision.gameObject.tag != "Wall") {
+//			
+//			Debug.Log (collision.gameObject.tag);
+//		}
+//	}
+}
