089183022bd9a2b43f5b976da29921bd729e5ad5
ShimiShun <b1013077@gmail.com>
2018-01-09 17:58:39 +0900

    中心視野に見せる簡単な計算問題作成

---
+++ Assets/3D-MOT/Script/SimpleRandomCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SimpleRandomCalculator : MonoBehaviour {
+
+	[SerializeField]
+	private Text text1;
+	[SerializeField]
+	private Text text2;
+	[SerializeField]
+	private Text CalcText;
+
+	private float TimerCount = 0;
+	private string[] CalcMeth = { "+", "ー" };
+	private int a = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		text1.text = Random.Range (10, 51).ToString ();
+		text2.text = Random.Range (10, 51).ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		TimerCount += Time.deltaTime;
+		//Debug.Log (TimerCount);
+
+		if (TimerCount >= a) {
+			text1.text = Random.Range (10, 51).ToString ();
+			text2.text = Random.Range (10, 51).ToString ();
+
+			if (a % 4 == 0) {
+				CalcText.text = CalcMeth [0];
+			}else{
+				CalcText.text = CalcMeth [1];
+			}
+
+			a += 10;
+		}
+
+	}
+
+
+}
